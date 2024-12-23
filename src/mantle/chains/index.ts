63afb2ba6501769b4c42a639818389beb2633ec5
dennisloh95 <dennisloh95@gmail.com>
2024-12-23 16:58:56 +0800

    chore: ini viem/op-stack

--- src/mantle/chains/index.ts
+++
@@ -1,2 +0,0 @@
-export { mantle } from "./mantle.js";
-export { mantleSepoliaTestnet } from "./mantleSepoliaTestnet.js";
--- src/mantle/types/l2Actions.ts
+++
@@ -1,6 +0,0 @@
-import { type Address, zeroAddress } from "viem";
-
-export const parseWithdrawL2Token = (l2Token?: Address) => {
-	if (!l2Token) return zeroAddress;
-	return l2Token;
-};
